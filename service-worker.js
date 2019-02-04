866d61b4b2cee18a4f0a5648d02f96d981b28a5c
Ahad Shaikh <ahad.shaikh0507.com>
2019-02-04 15:59:42 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Titosverify/precache-manifest.a7587b0500b31c16f7cb51b9c10b6179.js"
+  "/Titosverify/precache-manifest.3f74f622563f5cc2f8028b4dc75c0b8d.js"
 );
 
 workbox.clientsClaim();

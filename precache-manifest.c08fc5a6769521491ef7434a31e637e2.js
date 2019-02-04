81f0ba7aebbcb8f1808c350d8a2b4635df368cbc
Ahad Shaikh <ahad.shaikh0507.com>
2019-02-04 11:26:27 +0530

    Updates

---
+++ precache-manifest.c08fc5a6769521491ef7434a31e637e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Titosverify/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e6457f4fb64336db548b",
+    "url": "/Titosverify/static/js/runtime~main.e6457f4f.js"
+  },
+  {
+    "revision": "e22cec7b346ce298ca3e",
+    "url": "/Titosverify/static/js/main.e22cec7b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Titosverify/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "e22cec7b346ce298ca3e",
+    "url": "/Titosverify/static/css/main.198dd5a4.chunk.css"
+  },
+  {
+    "revision": "6325c595ed645beb77a57a910bcb9f56",
+    "url": "/Titosverify/index.html"
+  }
+];
